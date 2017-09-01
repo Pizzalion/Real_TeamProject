28a3ed16def9c9c9bbcc6f966d49983dc2e87199
SIST-js\sist41 <dieoxcin@naver.com>
2017-09-01 19:52:42 +0900

    Map PROTOTYPE

---
+++ finalproject/src/com/sist/mapsearch/dao/GPSVO.java
@@ -0,0 +1,19 @@
+package com.sist.mapsearch.dao;
+
+public class GPSVO {
+	private String x_p;
+	private String y_p;
+	
+	public String getX_p() {
+		return x_p;
+	}
+	public void setX_p(String x_p) {
+		this.x_p = x_p;
+	}
+	public String getY_p() {
+		return y_p;
+	}
+	public void setY_p(String y_p) {
+		this.y_p = y_p;
+	}
+}
---
+++ finalproject/src/com/sist/model/MapSearchModel.java
@@ -0,0 +1,23 @@
+package com.sist.model;
+
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+
+import com.sist.mapsearch.dao.GPSVO;
+
+public class MapSearchModel {
+	
+	public void MapSearch(HttpServletRequest request)
+	{
+		request.setAttribute("asdfasdf", "MapSearch/Map_Main.jsp");
+	}
+	
+	
+	//지도에 좌표점은 위도(x),경도(y)가 필요
+	public List<String> startMapSCH()
+	{
+		List<String> list = new ArrayList<String>();
+		
+		return list;
+	}
+}
