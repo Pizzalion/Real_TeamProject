6c2c619832efdc857ff3d0b5a27612521a4e5802
sist22\bae <morendo1213@gmail.com>
2017-09-02 15:02:25 +0900

    InnerHallVO add

--- finalproject/src/com/sist/wedding/dao/HallVO.java
+++ finalproject/src/com/sist/wedding/dao/HallVO.java
@@ -1,5 +1,73 @@
 package com.sist.wedding.dao;
-/* È¦ Á¤º¸ */
-public class HallVO {
 
+public class HallVO {
+	private String com_name,com_addr,hall_style,hall_fmenu,food_price,people,hall_image;
+	private int xpos,ypos,zpos,com_no;
+	public String getCom_name() {
+		return com_name;
+	}
+	public void setCom_name(String com_name) {
+		this.com_name = com_name;
+	}
+	public String getCom_addr() {
+		return com_addr;
+	}
+	public void setCom_addr(String com_addr) {
+		this.com_addr = com_addr;
+	}
+	public String getHall_style() {
+		return hall_style;
+	}
+	public void setHall_style(String hall_style) {
+		this.hall_style = hall_style;
+	}
+	public String getHall_fmenu() {
+		return hall_fmenu;
+	}
+	public void setHall_fmenu(String hall_fmenu) {
+		this.hall_fmenu = hall_fmenu;
+	}
+	public String getFood_price() {
+		return food_price;
+	}
+	public void setFood_price(String food_price) {
+		this.food_price = food_price;
+	}
+	public String getPeople() {
+		return people;
+	}
+	public void setPeople(String people) {
+		this.people = people;
+	}
+	public String getHall_image() {
+		return hall_image;
+	}
+	public void setHall_image(String hall_image) {
+		this.hall_image = hall_image;
+	}
+	public int getXpos() {
+		return xpos;
+	}
+	public void setXpos(int xpos) {
+		this.xpos = xpos;
+	}
+	public int getYpos() {
+		return ypos;
+	}
+	public void setYpos(int ypos) {
+		this.ypos = ypos;
+	}
+	public int getZpos() {
+		return zpos;
+	}
+	public void setZpos(int zpos) {
+		this.zpos = zpos;
+	}
+	public int getCom_no() {
+		return com_no;
+	}
+	public void setCom_no(int com_no) {
+		this.com_no = com_no;
+	}
 }
+
